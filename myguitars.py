dc2ccaa120622300e28564ca5725f98c7b9092a3
Sathvika-7284 <sathvika7276@gmail.com>
2022-12-25 03:08:17 +0800

    guitars appending to csv file

--- myguitars.py
+++ myguitars.py
@@ -11,7 +11,7 @@ VINTAGE_YEAR = 50
 class Guitar:
     """ Storing details of guitar """
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         """ Construct guitar type from the given values."""
         self.name = name
         self.year = year
@@ -19,7 +19,7 @@ class Guitar:
 
     def __str__(self):
         """ Return string representation of a guitar."""
-        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+        return f"{self.name}, {self.year}, {self.cost}"
 
     def get_age(self):
         """ Get the age of guitar using the current year"""
@@ -28,3 +28,31 @@ class Guitar:
     def is_vintage(self):
         """ Check if guitar is older than vintage year """
         return self.get_age() >= VINTAGE_YEAR
+
+    def __lt__(self, other):
+        """ Sorting guitars by year released """
+        return self.year < other.year
+
+
+def main():
+    guitars_list = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        add_guitar = Guitar(name, year, cost)
+        guitars_list.append([add_guitar])
+        print(add_guitar)
+        name = input("Name: ")
+
+    from csv import writer, QUOTE_NONE
+    with open('guitars.csv', 'a', newline='') as f_object:
+        writer_object = writer(f_object, quoting=QUOTE_NONE, escapechar=" ")
+        f_object.write("\n")
+        writer_object.writerows(guitars_list)
+        f_object.close()
+
+
+if __name__ == "__main__":
+    main()
